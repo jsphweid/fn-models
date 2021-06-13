70b9a693a108b03dad1a812920049bdc1faeb6fa
jsphweid <jsphweid@gmail.com>
2021-06-13 13:18:21 -0500

    use torchserve

--- backsync-models.sh
+++ backsync-models.sh
@@ -1,1 +1,9 @@
+find . -name ".DS_Store" -delete
+
+# push any new stuff
+aws s3 sync models s3://$S3_MODEL_BUCKET_NAME
+
+# pull any old stuff
 aws s3 sync s3://$S3_MODEL_BUCKET_NAME models
+
+# what could possibly go wrong
